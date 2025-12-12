3d6f052b3ab1b37e992dfb3f849f7d7cf79b0d17
Leonardo Urbina <apinky549@gmail.com>
2025-12-12 00:42:31 -0500

    style: newline at end of files and clean up

--- lib/loadenv.lua
+++ lib/loadenv.lua
@@ -1,28 +1,28 @@
 function load(path)
-  path = path or ".env"
+    path = path or '.env'
+    local f, err = io.open(path, 'r')
 
-  local f, err = io.open(path, "r")
-  if not f then
-    return nil, err
-  end
-
-  for line in f:lines() do
-    line = line:match("^%s*(.-)%s*$")
-
-    if line ~= "" and not line:match("^#") then
-      local key, value = line:match("^([%w_]+)%s*=%s*(.*)$")
-
-      if key then
-        value = value:gsub("^(['\"])(.*)%1$", "%2")
-        if _G.process and process.env then
-          process.env[key] = value
-        end
-      end
+    if not f then
+        return nil, err
     end
-  end
 
-  f:close()
-  return true
+    for line in f:lines() do
+        line = line:match('^%s*(.-)%s*$')
+
+        if line ~= "" and not line:match("^#") then
+            local key, value = line:match("^([%w_]+)%s*=%s*(.*)$")
+
+            if key then
+                value = value:gsub("^(['\"])(.*)%1$", "%2")
+
+                if _G.process and process.env then
+                    process.env[key] = value
+                end
+            end
+        end
+    end
+    f:close()
+    return true
 end
 
 return load
